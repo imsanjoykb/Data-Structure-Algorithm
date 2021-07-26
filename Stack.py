b55f4d5637aeabb8a409067d263505fb5d609371
imsanjoykb <sanjoy.eee32@gmail.com>
2021-07-26 09:57:28 +0600

    Stack Ds Updated

--- Stack.py
+++ Stack.py
@@ -9,7 +9,7 @@ pop() – Deletes the top most element of the stack – Time Complexity : O(1)
 '''
 
 #Implementation using list:
-
+print('----------Implementation using list-------------')
 stack = []
 
 stack.append('January')
@@ -30,4 +30,46 @@ print(stack)
 
 print(stack.pop())
 print(stack)
+
+
+'''Implementation using collections.deque'''
+print('----------Implementation using collections.deque-------------')
+from collections import deque
+
+stack_de = deque()
+
+stack_de.append('Book')
+stack_de.append('pen')
+stack_de.append('College')
+stack_de.append('University')
+
+print(stack_de)
+
+print(stack_de.pop())
+print(stack_de)
+
+print(stack_de.pop())
+print(stack_de)
+
+print(stack_de.pop())
+print(stack_de)
+
+print(stack_de.pop())
+print(stack_de)
+
+
+'''Exaple Of Stack Implementation'''
+print('-------------Exaple Of Stack Implementation---------------')
+
+class stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    
 
