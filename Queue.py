69b8e6706d766c77b97ee686be3458cc8bb0dade
imsanjoykb <sanjoy.eee32@gmail.com>
2021-07-27 10:23:18 +0600

    Queue Updated 2

--- Queue.py
+++ Queue.py
@@ -61,5 +61,56 @@ print(q.popleft())
 print(q)
 
 
+'''Implementation using queue.Queue'''
+print('-------------Implementation using queue.Queue--------------')
+
+from queue import Queue
+
+qq = Queue(maxsize=4)
+
+print(qq.qsize())
+
+qq.put('a')
+qq.put('b')
+qq.put('c')
+qq.put('d')
+
+print(qq.full())
+
+
+'''Queue Implementation'''
+print('----------------Queue Implementation---------------')
+
+# Implement Queue using List(Functions)
+q=[]
+def Enqueue():
+    if len(q)==size: # check wether the stack is full or not
+        print("Queue is Full!!!!")
+    else:
+        element=input("Enter the element:")
+        q.append(element)
+        print(element,"is added to the Queue!")
+def dequeue():
+    if not q:# or if len(stack)==0
+        print("Queue is Empty!!!")
+    else:
+        e=q.pop(0)
+        print("element removed!!:",e)
+def display():
+    print(q)
+    size=int(input("Enter the size of Queue:"))
+    while True:
+        print("Select the Operation:1.Add 2.Delete 3. Display 4. Quit")
+        choice=int(input())
+        if choice==1:
+            Enqueue()
+        elif choice==2:
+            dequeue()
+        elif choice==3:
+            display()
+        elif choice==4:
+            break
+        else:
+            print("Invalid Option!!!")
 
 
